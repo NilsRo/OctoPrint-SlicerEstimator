08773887d626201349c7927720b2f211e098bbf6
NilsRo <n.rottgardt@gmail.com>
2022-01-07 13:33:49 +0100

    Changed .plugin description

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ plugin_version = "1.1.2"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = "Estimate printing time from Slicer embedded info"
+plugin_description = "Use Slicer embedded info like estimated printtime, material name,..."
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Nils Hendrik Rottgardt"
